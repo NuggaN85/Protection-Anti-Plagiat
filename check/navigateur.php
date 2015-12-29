e1d439ccaf1dd108798e5c0d1e7a798b8713c837
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2015-12-29 18:15:54 +0100

    Create navigateur.php

---
+++ check/navigateur.php
@@ -0,0 +1,48 @@
+if (preg_match_all("#Opera (.*)(\[[a-z]{2}\];)?$#isU", $_SERVER["HTTP_USER_AGENT"], $version))
+{
+	$navigateur = 'Opéra ' . $version[1][0];
+}
+elseif (preg_match_all("#MSIE (.*);#isU", $_SERVER["HTTP_USER_AGENT"], $version))
+{
+	$navigateur = 'Internet Explorer ' . $version[1][0];
+}
+elseif (preg_match_all("#Firefox(.*)$#isU", $_SERVER["HTTP_USER_AGENT"], $version))
+{
+	$version = str_replace('/', '', $version[1][0]);
+	$navigateur = 'Firefox ' . $version;
+}
+elseif (preg_match_all("#Chrome(.*) Safari#isU", $_SERVER["HTTP_USER_AGENT"], $version))
+{
+	$version = str_replace('/', '', $version[1][0]);
+	$navigateur = 'Chrome ' . $version;
+}
+elseif (preg_match_all("#Opera(.*) \(#isU", $_SERVER["HTTP_USER_AGENT"], $version))
+{
+	$version = str_replace('/', '', $version[1][0]);
+	$navigateur = 'Opéra ' . $version;
+}
+elseif (preg_match("#Nokia#", $_SERVER["HTTP_USER_AGENT"]))
+{
+	$navigateur = 'Nokia';
+}
+elseif (preg_match("#Safari#", $_SERVER["HTTP_USER_AGENT"]))
+{
+	$navigateur = 'Safari';
+}
+elseif (preg_match("#SeaMonkey#", $_SERVER["HTTP_USER_AGENT"]))
+{
+	$navigateur = 'SeaMonkey';
+}
+elseif (preg_match("#PSP#", $_SERVER["HTTP_USER_AGENT"]))
+{
+	$navigateur = 'PSP';
+}
+elseif (preg_match("#Netscape#", $_SERVER["HTTP_USER_AGENT"]))
+{
+	$navigateur = 'Netscape';
+}
+else
+{
+	$navigateur = 'Inconnu';
+}
+
