49d1ce43bb4961dda1410728d23e8fcc5b838f48
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2015-12-30 14:16:35 +0100

    Update navigateur.php

--- check/navigateur.php
+++ check/navigateur.php
@@ -1,6 +1,13 @@
 <?php
+/*!
+* protectionantiplagiat init v1.0
+* Dev: NuggaN85
+* Github: NuggaN85
+* Twitter: @NuggaN85
+* Copyright © 2015 All rights reserved.
+* Licensed under CC BY 3.0
+*/
 function getnav() {
-
 if (preg_match_all("#Opera (.*)(\[[a-z]{2}\];)?$#isU", $_SERVER["HTTP_USER_AGENT"], $version))
 {
 	$navigateur = 'Opéra ' . $version[1][0];
