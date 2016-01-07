9a47ac7f11142a0ee0848ff82488bf474fa2adef
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-07 23:37:15 +0100

    Update protectionantiplagiat.php

--- protectionantiplagiat.php
+++ protectionantiplagiat.php
@@ -51,7 +51,7 @@ function geturl() {
 //------------------------------------------
 $user_agent = $_SERVER['HTTP_USER_AGENT'] . "\n";
 $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-if(in_array("$user_agent", $bannav)){ die(); }
+if(in_array("$user_agent", $bannav)){ die(); } // Liste des aspirateur en fichier externe 'bannav.php'
 foreach ($bannav as $banned) { $comparaison = strstr($user_agent, $banned);
     if($comparaison !== false) {
        $write_this = '[Information] Aspirateur : '.$user_agent.' Host : '.$hostname.' Adresse ip : ' .getip(); // Le texte que vous voulez avoir dans votre fichier protectionantiplagiat.cnx
