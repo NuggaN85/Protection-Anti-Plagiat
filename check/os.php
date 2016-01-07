48bcb262d07f1c725d2b7d4c94fe771e8adb823a
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-07 23:19:19 +0100

    Update os.php

--- check/os.php
+++ check/os.php
@@ -7,9 +7,9 @@
 * Copyright © 2015 All rights reserved.
 * Licensed under CC BY 3.0
 */
-if (preg_match_all("#Windows NT (.*)[;|\)]#isU", $_SERVER["HTTP_USER_AGENT"], $version))
+if(preg_match_all("#Windows NT (.*)[;|\)]#isU", $_SERVER["HTTP_USER_AGENT"], $version))
 {
-	if ($version[1][0] == '6.1')
+	if($version[1][0] == '6.1')
 	{
 		$os = 'Windows Seven';
 	}
@@ -30,39 +30,39 @@ if (preg_match_all("#Windows NT (.*)[;|\)]#isU", $_SERVER["HTTP_USER_AGENT"], $v
 		$os = 'Windows ' . $version[1][0];
 	}
 }
-elseif (preg_match_all("#Mac (.*);#isU", $_SERVER["HTTP_USER_AGENT"], $version))
+elseif(preg_match_all("#Mac (.*);#isU", $_SERVER["HTTP_USER_AGENT"], $version))
 {
 	$os = 'Mac ' . $version[1][0];
 }
-elseif (preg_match("#Windows 98#", $_SERVER["HTTP_USER_AGENT"]))
+elseif(preg_match("#Windows 98#", $_SERVER["HTTP_USER_AGENT"]))
 {
 	$os = 'Windows 98';
 }
-elseif (preg_match("#Mac#", $_SERVER["HTTP_USER_AGENT"]))
+elseif(preg_match("#Mac#", $_SERVER["HTTP_USER_AGENT"]))
 {
 	$os = 'Mac';
 }
-elseif (preg_match("#SunOS#", $_SERVER["HTTP_USER_AGENT"]))
+elseif(preg_match("#SunOS#", $_SERVER["HTTP_USER_AGENT"]))
 {
 	$os = 'SunOS';
 }
-elseif (preg_match("#Fedora#", $_SERVER["HTTP_USER_AGENT"]))
+elseif(preg_match("#Fedora#", $_SERVER["HTTP_USER_AGENT"]))
 {
 	$os = 'Fedora';
 }
-elseif (preg_match("#Haiku#", $_SERVER["HTTP_USER_AGENT"]))
+elseif(preg_match("#Haiku#", $_SERVER["HTTP_USER_AGENT"]))
 {
 	$os = 'Haiku';
 }
-elseif (preg_match("#Ubuntu#", $_SERVER["HTTP_USER_AGENT"]))
+elseif(preg_match("#Ubuntu#", $_SERVER["HTTP_USER_AGENT"]))
 {
 	$os = 'Linux Ubuntu';
 }
-elseif (preg_match("#FreeBSD#", $_SERVER["HTTP_USER_AGENT"]))
+elseif(preg_match("#FreeBSD#", $_SERVER["HTTP_USER_AGENT"]))
 {
 	$os = 'FreeBSD';
 }
-elseif (preg_match("#Linux#", $_SERVER["HTTP_USER_AGENT"]))
+elseif(preg_match("#Linux#", $_SERVER["HTTP_USER_AGENT"]))
 {
 	$os = 'Linux';
 }
