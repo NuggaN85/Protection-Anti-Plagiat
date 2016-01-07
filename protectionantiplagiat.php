5dc0fa248895fbdc70d7d86a6c18b3549b8628e4
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-07 23:44:25 +0100

    Update protectionantiplagiat.php

--- protectionantiplagiat.php
+++ protectionantiplagiat.php
@@ -51,10 +51,10 @@ function geturl() {
 //------------------------------------------
 $user_agent = $_SERVER['HTTP_USER_AGENT'] . "\n";
 $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-if(in_array("$user_agent", $bannav)){ die(); } // Liste des aspirateur en fichier externe 'bannav.php'
+if(in_array("$user_agent", $bannav)){ die(); } // Liste des aspirateurs en fichier externe 'bannav.php'.
 foreach ($bannav as $banned) { $comparaison = strstr($user_agent, $banned);
     if($comparaison !== false) {
-       $write_this = '[Information] Aspirateur : '.$user_agent.' Host : '.$hostname.' Adresse ip : ' .getip(); // Le texte que vous voulez avoir dans votre fichier protectionantiplagiat.cnx
+       $write_this = '[Information] Aspirateur : '.$user_agent.' Host : '.$hostname.' Adresse ip : ' .getip(); // Le texte que vous voulez avoir dans votre fichier protectionantiplagiat.cnx.
        $tentative++;
     }
 }
@@ -62,10 +62,10 @@ foreach ($bannav as $banned) { $comparaison = strstr($user_agent, $banned);
 // récuperation des infos avec fichier auto écrit
 //----------------------------------------------------
 if($tentative > 0){
-   $write_here = fopen("protectionantiplagiat.cnx", "a"); // Fichier cnx auto inclus a la racine avec le protectionantiplagiat.php
+   $write_here = fopen("protectionantiplagiat.cnx", "a"); // Fichier cnx auto inclus a la racine avec le protectionantiplagiat.php.
    fwrite($write_here, "\n" . $write_this);
    fclose($write_here);
-   echo utf8_decode( '[Sécurité] Notre site web est protégé contre le vole et le spam, vos information serons automatiquement bannie sur la base de donnée de projecthoneypot <br><br> [Information] : '.$user_agent.' '.$hostname.' '.getip().''); // Le texte que vous voulez que le voleur recevra dans les fichiers télécharger	
+   echo utf8_decode( '[Sécurité] Notre site web est protégé contre le vole et le spam, vos information serons automatiquement bannie sur la base de donnée de projecthoneypot <br><br> [Information] : '.$user_agent.' '.$hostname.' '.getip().''); // Le texte que vous voulez que le voleur recevra dans les fichiers télécharger.	
 //-------------------------
 // Notification emails
 //-------------------------
