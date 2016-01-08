79f81b9d1f077763d8caf397e5fe1f5c7285e99f
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-09 00:00:29 +0100

    Update autoload.php

--- check/autoload.php
+++ check/autoload.php
@@ -4,7 +4,9 @@ require_once($base_dir.'os.php');
 require_once($base_dir.'navigateur.php');
 require_once($base_dir.'bannav.php');
 
-if (file_exists($base_dir)) {
-    require('protectionantiplagiat.php');
-  }
+function __autoload($base_dir)
+{
+    require('protectionantiplagiat.php', $base_dir);
+}
+if(!file_exist('protectionantiplagiat.php'){ die(); }
 ?>
