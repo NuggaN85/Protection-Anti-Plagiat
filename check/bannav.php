5413254a2823f9ef41389c3ddc8126307c3734ca
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-09 19:56:19 +0100

    Update bannav.php

--- check/bannav.php
+++ check/bannav.php
@@ -1,12 +1,4 @@
 <?php
-/*!
-* protectionantiplagiat init v1.0
-* Dev: NuggaN85
-* Github: NuggaN85
-* Twitter: @NuggaN85
-* Copyright © 2015 All rights reserved.
-* Licensed under CC BY 3.0
-*/
 $bannav = Array(
   'HTTrack',
   'httrack',
