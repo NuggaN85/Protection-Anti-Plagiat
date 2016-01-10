b0c9a4f49a605ab9bca9789cb7153e66c291bdce
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-10 14:18:43 +0100

    Update data.php

--- check/data.php
+++ check/data.php
@@ -5,4 +5,9 @@ require_once($base_dir.'os.php');
 require_once($base_dir.'navigateur.php');
 require_once($base_dir.'email.php');
 //==========
+
+//===== Futur update
+//$JSON = file_get_contents("bannav.json");
+//$JSON = json_decode($JSON);
+//var_dump($JSON);
 ?>
