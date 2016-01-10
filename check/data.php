228646e6e29654bc88bda91ba0e673bb56edf0f9
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-10 01:01:59 +0100

    Update data.php

--- check/data.php
+++ check/data.php
@@ -1,5 +1,6 @@
 <?php
+require_once($base_dir.'bannav.php');
 require_once($base_dir.'os.php');
 require_once($base_dir.'navigateur.php');
-require_once($base_dir.'bannav.php');
+require_once($base_dir.'email.php');
 ?>
