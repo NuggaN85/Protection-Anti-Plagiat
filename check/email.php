e7fe4cc067a42a46b7acda56d824f914ee575ad1
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-10 01:43:18 +0100

    Update email.php

--- check/email.php
+++ check/email.php
@@ -1,12 +1,44 @@
 <?php
-$email_expediteur = 'noreply@protection-anti-plagiat.com'; 
-$email_reply = 'noreply@protection-anti-plagiat.com';
-$headers = 'From: "PAP.COM" <'.$email_expediteur.'>'."\n"; 
-$headers .= 'Return-Path: <'.$email_reply.'>'."\n";
-$headers .= 'Content-Type: text/html; charset="iso-8859-1"'."\r\n";
-$headers .= 'Content-Transfer-Encoding: 8bit';
-$destinataire .= 'VOTRE EMAIL';
-$sujet .= '[protection-anti-plagiat]';
-$message = ''.$useragent.' '.$os.' '.$navigateur.'';
-if(mail($destinataire,$sujet,$message,$headers))
+$mail = 'VOTRE EMAIL'; // Déclaration de l'adresse de destination.
+if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
+{
+	$passage_ligne = "\r\n";
+}
+else
+{
+	$passage_ligne = "\n";
+}
+//=====Déclaration des messages au format texte et au format HTML.
+$message_txt = ".$useragent.' '.$os.' '.$navigateur.";
+//==========
+ 
+//=====Création de la boundary
+$boundary = "-----=".md5(rand());
+//==========
+ 
+//=====Définition du sujet.
+$sujet = "[protection-anti-plagiat]";
+//=========
+ 
+//=====Création du header de l'e-mail.
+$header = "From: \"PAP.COM\"<noreply@protection-anti-plagiat.com>".$passage_ligne;
+$header.= "Reply-to: \"PAP.COM\" <noreply@protection-anti-plagiat.com>".$passage_ligne;
+$header.= "MIME-Version: 1.0".$passage_ligne;
+$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+//==========
+ 
+//=====Création du message.
+$message = $passage_ligne."--".$boundary.$passage_ligne;
+//=====Ajout du message au format texte.
+$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_txt.$passage_ligne;
+//==========
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+//==========
+ 
+//=====Envoi de l'e-mail.
+mail($mail,$sujet,$message,$header);
+//==========
 ?>
