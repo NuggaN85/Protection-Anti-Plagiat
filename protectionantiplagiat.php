6cee95573a36fd7a85a04ed36351fd2fd47c4fa2
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-10 02:26:59 +0100

    Update protectionantiplagiat.php

--- protectionantiplagiat.php
+++ protectionantiplagiat.php
@@ -1,9 +1,9 @@
 <?php
-//=====récuperation des fichier externe
+//===== Récuperation des fichier externe
 require('data.php');
 //==========
 
-//=====récuperation des ip v4 & v6 du client 
+//===== Récuperation des ip v4 & v6 du client 
 function getip() {
     $ipaddress = '';
     if(getenv('HTTP_CLIENT_IP'))
@@ -24,7 +24,7 @@ function getip() {
 }
 //==========
 
-//=====donnée des attaques de bot aspirateur
+//===== Donnée des attaques de bot aspirateur
 $useragent = $_SERVER['HTTP_USER_AGENT'] . "\n";
 if(in_array("$useragent", $bannav)){ die(); } // Liste des aspirateurs en fichier externe 'bannav.php'.
 foreach ($bannav as $banned) { $comparaison = strstr($useragent, $banned);
@@ -34,7 +34,7 @@ foreach ($bannav as $banned) { $comparaison = strstr($useragent, $banned);
 }
 //==========
 
-//=====récuperation des infos avec fichier auto écrit
+//===== Récuperation des infos avec fichier auto écrit
 if($tentative > 0){
    $files = fopen("protectionantiplagiat.cnx", "a"); // Fichier cnx auto inclus a la racine avec le protectionantiplagiat.php.
    $log = '[Information] Aspirateur : '.$useragent.' Adresse ip : ' .getip(); // Le texte que vous voulez avoir dans votre fichier protectionantiplagiat.cnx.
