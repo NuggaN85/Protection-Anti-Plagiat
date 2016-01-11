3550e4e96d42462eba62651697e90cfaf4c24d18
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-11 01:15:33 +0100

    Update data.php

--- check/data.php
+++ check/data.php
@@ -1,8 +1,6 @@
 <?php
 //===== Les includes des fichier externe
 require_once($base_dir.'bannav.php');
-require_once($base_dir.'os.php');
-require_once($base_dir.'navigateur.php');
 require_once($base_dir.'email.php');
 //==========
 ?>
