a6365da7261d7724747aa1e1997ae0a3872997b3
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-12 15:08:23 +0100

    Create email.php

---
+++ pap/check/email.php
@@ -0,0 +1,45 @@
+<?php
+$mail = 'VOTRE MAIL'; // Déclaration de l'adresse de destination.
+if (!preg_match("#^[a-z0-9._-]+@(hotmail|msn|yahoo).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
+{
+	$passage_ligne = "\r\n";
+}
+else
+{
+	$passage_ligne = "\n";
+}
+//=====Déclaration des messages au format html.
+$message_html = "[INFORMATION CLIENT] <br><br>".$user." URL : ".geturl()." <br><br>IP CLIENT : ".getip()." <br><br>PORT CLIENT : ".$client_port." <br><br>HOST CLIENT : ".$hostname."
+<br><br>[INFORMATION HEBERGEUR] <br><br>IP SERVEUR : ".$server_ip." <br><br>ISP : ".getisp($isp)." <br><br><br>POUR PLUS AVOIR DE SOUCI VEUILLEZ BLOQUER LE CLIENT DANS LA BLACKLIST.";
+//==========
+ 
+//=====Création de la boundary
+$boundary = "-----=".md5(rand());
+//==========
+ 
+//=====Définition du sujet.
+$sujet = "[protection-anti-plagiat]";
+//=========
+ 
+//=====Création du header de l'e-mail.
+$header = "From: \"PAP.COM\"<noreply@protection-anti-plagiat.com>".$passage_ligne;
+$header.= "Reply-to: \"PAP.COM\" <noreply@protection-anti-plagiat.com>".$passage_ligne;
+$header.= "MIME-Version: 1.0".$passage_ligne;
+$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+//==========
+ 
+//=====Création du message.
+$message = $passage_ligne."--".$boundary.$passage_ligne;
+//=====Ajout du message au format HTML
+$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_html.$passage_ligne;
+//==========
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+//==========
+ 
+//=====Envoi de l'e-mail.
+if(mail($mail,$sujet,$message,$header))
+//==========
+?>
