12b3e721f1dff6ba5555c29aa3833a7450d43722
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-29 10:45:59 +0100

    Update email.php

--- papprotect/check/email.php
+++ papprotect/check/email.php
@@ -22,8 +22,8 @@ $sujet = "[papprotect]";
 //=========
  
 //=====Création du header de l'e-mail.
-$header = "From: \"PAP.COM\"<noreply@protection-anti-plagiat.com>".$passage_ligne;
-$header.= "Reply-to: \"PAP.COM\" <noreply@protection-anti-plagiat.com>".$passage_ligne;
+$header = "From: \"PAP.COM\"<noreply@papprotect.com>".$passage_ligne;
+$header.= "Reply-to: \"PAP.COM\" <noreply@papprotect.com>".$passage_ligne;
 $header.= "MIME-Version: 1.0".$passage_ligne;
 $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
 //==========
