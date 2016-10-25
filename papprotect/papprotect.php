77d4af91317344dd9140efda2825a99f80eb6ab4
Rose Ludovic <NuggaN85@users.noreply.github.com>
2016-10-25 21:10:27 +0200

    Update papprotect.php

--- papprotect/papprotect.php
+++ papprotect/papprotect.php
@@ -1,6 +1,6 @@
 <?php
 //===== Récuperation des fichiers externe.
-require($base_dir.'check/data.php'); 
+require($base_dir.'check/data.php');
 //==========
 
 //===== Récuperation des ip v4 & v6 du client.
