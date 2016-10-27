da850c29e29c4eb1f098b98b88bc5005e92a5625
Rose Ludovic <NuggaN85@users.noreply.github.com>
2016-10-27 23:20:25 +0200

    Update papprotect.php

--- papprotect/papprotect.php
+++ papprotect/papprotect.php
@@ -50,7 +50,9 @@ if (!function_exists('file_put_contents')) {
    flock($files, LOCK_SH);
    fclose($files);
 // Le texte que vous voulez que le voleur recevra dans les fichiers télécharger.
-   echo utf8_decode('Notre site web est protégé, vos information sont enregistrer, ['.$ua.'] ['.getip().']');
+   echo utf8_decode('Site web est protégé, vos information sont enregistrer, ['.$ua.'] ['.getip().']');
+    
+   echo utf8_decode('Website is protected, your information is recorded, ['.$ua.'] ['.getip().']');
 //==========
    die(); } 
 ?>
