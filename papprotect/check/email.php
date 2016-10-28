758a56b73dc084600c2f099bc3b6c801b0f97a1e
Rose Ludovic <NuggaN85@users.noreply.github.com>
2016-10-28 02:11:37 +0200

    Update email.php

--- papprotect/check/email.php
+++ papprotect/check/email.php
@@ -1,10 +1,12 @@
 <?php
+// On filtre les serveurs qui rencontrent des bogues
+if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $destinataire)) // On filtre les serveurs qui rencontrent des bogues.
 //==========
 $destinataire = 'VOTRE EMAIL';
 // Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
 $expediteur = 'noreply@papprotect.com';
-$copie = 'papprotect@pap.com';
-$copie_cachee = 'papprotect@pap.com';
+$copie = 'noreply@papprotect.com';
+$copie_cachee = 'noreply@papprotect.com';
 $objet = '[PAPPROTECT]'; // Objet du message
 $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
 $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
@@ -12,18 +14,9 @@ $headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
 $headers .= 'From: "noreply@papprotect.com"<'.$expediteur.'>'."\n"; // Expediteur
 $headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
 $headers .= 'Cc: '.$copie."\n"; // Copie Cc
-$headers .= 'Bcc: '.$copie_cachee."\n\n"; // Copie cachée Bcc        
-$message = '<div style="width: 100%; text-align: center; font-weight: bold">[INFORMATION CLIENT] <br><br>".$ua." <br><br>IP CLIENT : ".getip()." <br><br>PORT CLIENT : ".$up." <br><br>HOST CLIENT : ".$uh."
+$headers .= 'Bcc: '.$copie_cachee."\n\n"; // Copie cachée Bcc
+$message = '<div style="width: 100%; text-align: center; font-weight: bold">[INFORMATION CLIENT] <br><br>'.$ua.' <br><br>IP CLIENT : '.getip().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$uh.'
 <br><br><br>POUR PLUS AVOIR DE SOUCI VEUILLEZ BLOQUER LE CLIENT DANS LA BLACKLIST.</div>';
 if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
 //==========
-	
-// On filtre les serveurs qui rencontrent des bogues
-function verifEmail ($destinataire) {
-    if (filter_var ($destinataire, FILTER_VALIDATE_EMAIL) === false) {
-        return false;
-    } else {
-        return true;
-    }
-}
 ?>
