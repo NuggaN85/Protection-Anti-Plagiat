29d873109e55d41a8bfe7e518258c1a5b9ee9a42
Rose Ludovic <NuggaN85@users.noreply.github.com>
2017-02-26 20:38:45 +0100

    Update data.php

--- papprotect/check/data.php
+++ papprotect/check/data.php
@@ -1,7 +1,7 @@
 <?php
 //===== Les includes des fichier externe
 if (file_exists($base_dir.'papprotect')) {
-  require_once($base_dir.'bannav.php');
+  require_once($base_dir.'bad_bots.php');
 } else {
   die('S\'il vous plaît mettre les fichiers dans le répertoire papprotect !');
 }
