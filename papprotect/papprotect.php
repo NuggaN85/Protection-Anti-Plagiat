f78a37052a51dcc9932627754b048d068b360d02
Rose Ludovic <NuggaN85@users.noreply.github.com>
2017-02-27 11:29:18 +0100

    Update papprotect.php

--- papprotect/papprotect.php
+++ papprotect/papprotect.php
@@ -7,7 +7,7 @@ if (file_exists($base_dir.'papprotect')) {
 }
 //==========
 //===== Récuperation des ip v4 & v6 du client.
-function getip() {
+function getUserIP() {
     $client  = getenv('HTTP_CLIENT_IP');
     $forward = getenv('HTTP_X_FORWARDED_FOR');
     $remote  = getenv('REMOTE_ADDR');
@@ -51,17 +51,17 @@ function getip() {
 // Fichier cnx auto inclus a la racine avec le papprotect.php.
    $files = fopen($base_dir."papprotect/papprotect-log.cnx", "a");
 // Le texte que vous voulez avoir dans votre fichier papprotect-log.cnx.
-   $log = ('['.$ua.'] ['.getip().'] ['.$up.'] ['.$ra.']');
+   $log = ('['.$ua.'] ['.getUserIP().'] ['.$up.'] ['.$ra.']');
    fputs($files, "\n" . $log);
    flock($files, LOCK_SH);
    fclose($files);
 // Le texte que vous voulez que le voleur recevra dans les fichiers télécharger.
    echo '<div style="text-align: center;"><img src="https://camo.githubusercontent.com/fe2cb3af77c3290cd9437c142662cbd08bbbc027/687474703a2f2f696d6167652e6e6f656c736861636b2e636f6d2f66696368696572732f323031352f35312f313435303130333535302d736865696c642e706e67" border="0" /></div> ';	
    echo '<br><br>';
-   echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Site web est protégé, vos information sont enregistrés] <br><br>'.$ua.' <br><br>IP CLIENT : '.getip().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
+   echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Site web est protégé, vos information sont enregistrés] <br><br>'.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
    </div>');
    echo '<br><br>';
-   echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Website is protected, your information is recorded] <br><br>'.$ua.' <br><br>IP CLIENT : '.getip().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
+   echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Website is protected, your information is recorded] <br><br>'.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
    </div>');
 //==========
 //Systeme de notification part Email.
@@ -84,7 +84,7 @@ function getip() {
    $headers .= 'Cc: '.$copie."\n"; // Copie Cc
    $headers .= 'Bcc: '.$copie_cachee."\n\n"; // Copie cachée Bcc
    $message = '<div style="width: 100%; text-align: center; font-weight: bold"><img src="https://camo.githubusercontent.com/fe2cb3af77c3290cd9437c142662cbd08bbbc027/687474703a2f2f696d6167652e6e6f656c736861636b2e636f6d2f66696368696572732f323031352f35312f313435303130333535302d736865696c642e706e67"><br><br>
-   SUR VOTRE SITEWEB : '.$sn.' <br><br>FICHIER ATTAQUER : '.$sf.' <br><br>'.$ua.' <br><br>IP CLIENT : '.getip().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
+   SUR VOTRE SITEWEB : '.$sn.' <br><br>FICHIER ATTAQUER : '.$sf.' <br><br>'.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
    <br><br><br>POUR PLUS AVOIR DE SOUCI VEUILLEZ BLOQUER LE CLIENT DANS LA BLACKLIST DE VOTRE SERVEUR.<br><br>
    <b><a href="https://github.com/NuggaN85/Protection-Anti-Plagiat/issues">Un bug? une suggestion?</a></b></div>';
 //==========   
