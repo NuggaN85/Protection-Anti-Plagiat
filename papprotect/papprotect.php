aa933d40c404650927ff077b43dd2675945b57dd
Rose Ludovic <NuggaN85@users.noreply.github.com>
2018-03-23 14:35:46 +0100

    Update papprotect.php

--- papprotect/papprotect.php
+++ papprotect/papprotect.php
@@ -1,10 +1,10 @@
 <?php
 //===== Récuperation des fichiers externe.
 if (file_exists($base_dir.'papprotect')) {
-  require($base_dir.'check/data.php');
+  require_once ($base_dir.'check/data.php');
 } else {
-  exit('S\'il vous plaît mettre les fichiers dans le répertoire papprotect!');
-}
+  echo utf8_encode('S\'il vous plaît mettre les fichiers dans le répertoire papprotect!');
+      exit(1); } 
 //==========
 //===== Récuperation des ip v4, v6 & cloud du client.
 function getUserIP() {
@@ -50,9 +50,9 @@ function getUserIP() {
        function file_put_contents($files) {  
        } 
    } 
-// Fichier papprotect-log auto inclus a la racine avec le papprotect.php. 
-   $files = fopen($base_dir."papprotect/papprotect-log", "a");
-// Le texte que vous voulez avoir dans votre fichier papprotect-log.
+// Fichier papprotect-log.cnx auto inclus a la racine du dossier papprotect. 
+   $files = fopen($base_dir."papprotect/papprotect-log.cnx", "a");
+// Le texte que vous voulez avoir dans votre fichier papprotect-log.cnx.
    $log = ('['.$ua.'] ['.getUserIP().'] ['.$up.'] ['.$ra.'] ['.$ss.']');
    fputs($files, "\n" . $log);
    flock($files, LOCK_SH);
@@ -60,10 +60,10 @@ function getUserIP() {
 // Le texte que vous voulez que le voleur recevra dans les fichiers télécharger.
    echo '<div style="text-align: center;"><img src="https://camo.githubusercontent.com/fe2cb3af77c3290cd9437c142662cbd08bbbc027/687474703a2f2f696d6167652e6e6f656c736861636b2e636f6d2f66696368696572732f323031352f35312f313435303130333535302d736865696c642e706e67" border="0" /></div> ';    
    echo '<br><br>';
-   echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Site web est protégé, vos information sont enregistrés] <br><br>'.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
+   echo utf8_encode('<div style="width: 100%; text-align: center; font-weight: bold">[Site web est protégé, vos information sont enregistrés] <br><br>'.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
    </div>');
    echo '<br><br>';
-   echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Website is protected, your information is recorded] <br><br>'.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
+   echo utf8_encode('<div style="width: 100%; text-align: center; font-weight: bold">[Website is protected, your information is recorded] <br><br>'.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'
    </div>');
 //==========
       exit(1); } 
