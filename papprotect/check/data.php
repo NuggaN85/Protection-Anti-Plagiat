431eb9de5e41be062ffd4523e096e6e7477b4214
Rose Ludovic <NuggaN85@users.noreply.github.com>
2018-04-22 22:21:53 +0200

    Update data.php

--- papprotect/check/data.php
+++ papprotect/check/data.php
@@ -3,7 +3,7 @@
 if (file_exists($base_dir.'papprotect')) {
   require_once ($base_dir.'bad_bots.php');
 } else {
-  die('S\'il vous plaît mettre les fichiers dans le répertoire papprotect !');
+  die('S\'il vous plaît mettre les fichiers dans le répertoire papprotect!');
 }
 //==========
 ?>
