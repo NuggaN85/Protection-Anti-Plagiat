f38c3b4a59e4bdf2891edfaed65472f781c8dae6
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-05 22:31:56 +0100

    Update papprotect.php

--- papprotect/papprotect.php
+++ papprotect/papprotect.php
@@ -71,14 +71,12 @@ if ($tentative > 0) {
     fclose($files);
   
 // Le texte que vous voulez que le voleur reçoive dans les fichiers téléchargés.
-   $message = "Votre téléchargement est en cours de traitement. Veuillez patienter...";
-   echo '<div style="text-align:center;"><img src="https://camo.githubusercontent.com/fe2cb3af77c3290cd9437c142662cbd08bbbc027/687474703a2f2f696d6167652e6e6f656c736861636b2e636f6d2f66696368696572732f323031352f35312f313435303130333535302d736865696c642e706e67" border="0" /></div> ";    
-   echo "<br><br>";
-   echo utf8_decode("<div style="width: 100%; text-align: center; font-weight: bold">[Site web est protégé, vos information sont enregistrés] <br><br>".$ss." ".$ua." <br><br>IP CLIENT : ".getUserIP()." <br><br>PORT CLIENT : ".$up." <br><br>HOST CLIENT : ".$ra."
-   </div>");
-   echo "<br><br>";
-   echo utf8_decode("<div style="width: 100%; text-align: center; font-weight: bold">[Website is protected, your information is recorded] <br><br>".$ss." ".$ua." <br><br>IP CLIENT : ".getUserIP()." <br><br>PORT CLIENT : ".$up." <br><br>HOST CLIENT : ".$ra."
-   </div>");
+$message = "Votre téléchargement est en cours de traitement. Veuillez patienter...";
+echo '<div style="text-align:center;"><img src="https://camo.githubusercontent.com/fe2cb3af77c3290cd9437c142662cbd08bbbc027/687474703a2f2f696d6167652e6e6f656c736861636b2e636f6d2f66696368696572732f323031352f35312f313435303130333535302d736865696c642e706e67" border="0" /></div>';
+echo "<br><br>";
+echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Site web est protégé, vos informations sont enregistrées] <br><br>'.$ss.' '.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'</div>');
+echo "<br><br>";
+echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Website is protected, your information is recorded] <br><br>'.$ss.' '.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'</div>');
 
       exit(); } 
 ?>
