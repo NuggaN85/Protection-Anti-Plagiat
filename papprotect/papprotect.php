a8a21bb01833905a874c91ddd958d9c8e8c46935
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-06 08:40:47 +0100

    Update papprotect.php

--- papprotect/papprotect.php
+++ papprotect/papprotect.php
@@ -72,7 +72,7 @@ if ($tentative > 0) {
   
 // Le texte que vous voulez que le voleur reçoive dans les fichiers téléchargés.
 $message = "Votre téléchargement est en cours de traitement. Veuillez patienter...";
-echo '<div style="text-align:center;"><img src="https://camo.githubusercontent.com/fe2cb3af77c3290cd9437c142662cbd08bbbc027/687474703a2f2f696d6167652e6e6f656c736861636b2e636f6d2f66696368696572732f323031352f35312f313435303130333535302d736865696c642e706e67" border="0" /></div>';
+echo '<div style="text-align:center;"><img src="https://camo.githubusercontent.com/e73fb5bda4df97a30987b09cae8cf58b6e76ac7f1ca41753671050be1a6e5c33/68747470733a2f2f65787465726e616c2d636f6e74656e742e6475636b6475636b676f2e636f6d2f69752f3f753d68747470732533412532462532467777772e666964656c697a617274652e7074253246626c6f6725324677702d636f6e74656e7425324675706c6f616473253246323031392532463039253246315f312d31303234783536312e706e6726663d31266e6f66623d31266970743d646631653837663661336238313133653333663135633463616161373863306333306438313630656132633933643039636131626435666565356338363661372669706f3d696d61676573" border="0" /></div>';
 echo "<br><br>";
 echo utf8_decode('<div style="width: 100%; text-align: center; font-weight: bold">[Site web est protégé, vos informations sont enregistrées] <br><br>'.$ss.' '.$ua.' <br><br>IP CLIENT : '.getUserIP().' <br><br>PORT CLIENT : '.$up.' <br><br>HOST CLIENT : '.$ra.'</div>');
 echo "<br><br>";
