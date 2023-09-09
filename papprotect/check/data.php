acec6bb33405bb336e025584ab4295e8fe4db0fd
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-09-09 18:55:45 +0200

    Update data.php
    Ces modifications rendent le code plus clair et maintenable. Assurez-vous également que le répertoire "papprotect" existe et contient le fichier "bad_bots.php".

--- papprotect/check/data.php
+++ papprotect/check/data.php
@@ -1,8 +1,11 @@
 <?php 
-//===== Les includes des fichiers externes
-$base_dir = __DIR__ . "/papprotect/"; // Initialiser la variable $base_dir avec le chemin du répertoire racine de l'application
-if (file_exists($base_dir . "bad_bots.php")) {
-  require_once($base_dir . "bad_bots.php");
+//===== Inclure les fichiers externes
+$base_dir = __DIR__ . "/papprotect/"; // Initialisez la variable $base_dir avec le chemin du répertoire racine de l'application
+
+// Vérifiez si le fichier bad_bots.php existe avant de l'inclure
+$bad_bots_file = $base_dir . "bad_bots.php";
+if (file_exists($bad_bots_file)) {
+  require_once($bad_bots_file);
 } else {
   die("Veuillez placer les fichiers dans le répertoire papprotect!");
 }
