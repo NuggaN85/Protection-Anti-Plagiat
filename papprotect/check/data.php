9e892403c12972defd1a96e6f62437eaa5e699c3
Rose Ludovic <NuggaN85@users.noreply.github.com>
2024-01-29 13:06:40 +0100

    Update data.php

--- papprotect/check/data.php
+++ papprotect/check/data.php
@@ -1,13 +1,17 @@
 <?php 
 //===== Inclure les fichiers externes
-$base_dir = __DIR__ . "/papprotect/"; // Initialisez la variable $base_dir avec le chemin du répertoire racine de l'application
+// Initialisez la variable $base_dir avec le chemin du répertoire racine de l'application
+$base_dir = __DIR__ . DIRECTORY_SEPARATOR . "papprotect" . DIRECTORY_SEPARATOR;
 
-// Vérifiez si le fichier bad_bots.php existe avant de l'inclure
+// Utilisez la constante DIRECTORY_SEPARATOR pour garantir la portabilité des chemins
 $bad_bots_file = $base_dir . "bad_bots.php";
+
+// Utilisez la fonction include_once() au lieu de require_once() si la non-existence du fichier n'est pas critique
 if (file_exists($bad_bots_file)) {
-  require_once($bad_bots_file);
+  include_once $bad_bots_file;
 } else {
-  die("Veuillez placer les fichiers dans le répertoire papprotect!");
+  // Utilisez exit() au lieu de die() pour une gestion plus souple des erreurs
+  exit("Veuillez placer les fichiers dans le répertoire papprotect!");
 }
 //==========
 ?>
